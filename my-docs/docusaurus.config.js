d93a43caa2843dfac72eeda3038529bf932efcc8
Gáldi-Takács Bertold <takacs.bertold@projectpartner.hu>
2025-12-14 21:58:15 +0100

    Feat: Configure GitHub Pages deployment and add CI/CD workflow

--- my-docs/docusaurus.config.js
+++ my-docs/docusaurus.config.js
@@ -19,16 +19,13 @@ const config = {
         v4: true, // Improve compatibility with the upcoming Docusaurus v4
     },
 
-    // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
-    // Set the /<baseUrl>/ pathname under which your site is served
-    // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    url: 'https://galditakacsbertold.github.io',
+    baseUrl: '/docusaurus_project/',
 
-    // GitHub pages deployment config.
-    // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'galditakacsbertold',
+    projectName: 'docusaurus_project',
+
+    trailingSlash: false,
 
     onBrokenLinks: 'warn',
     onBrokenMarkdownLinks: 'warn',
